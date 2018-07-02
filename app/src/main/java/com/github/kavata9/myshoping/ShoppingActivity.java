17a5428f3455a5911c5528fd0941bc3da907051d
kavata9 <kavata24@gmail.com>
2018-07-02 16:18:35 +0300

    add textview to shopping activity

--- app/src/main/java/com/github/kavata9/myshoping/ShoppingActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/ShoppingActivity.java
@@ -3,14 +3,17 @@ package com.github.kavata9.myshoping;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShoppingActivity extends Activity {
+    private TextView mProductTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
+        mProductTextView = (TextView) findViewById(R.id.productTextView);
         Intent intent = getIntent();
         String product = intent.getStringExtra("product");
     }
