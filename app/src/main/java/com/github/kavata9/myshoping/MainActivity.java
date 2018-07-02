ec6b5bb38e80f5da80a14316e8ce3a35d0978e59
kavata9 <kavata24@gmail.com>
2018-07-02 12:28:05 +0300

    add intent to the main function so as to move to the next activity

--- app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
@@ -1,6 +1,7 @@
 package com.github.kavata9.myshoping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ public class MainActivity extends Activity {
         mLetsshopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShoppingActivity.class);
+                startActivity(intent);
                 Toast.makeText(MainActivity.this, "Shopping Time!", Toast.LENGTH_LONG).show();
             }
         });
---
+++ app/src/main/java/com/github/kavata9/myshoping/ShoppingActivity.java
@@ -0,0 +1,8 @@
+package com.github.kavata9.myshoping;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingActivity extends Activity {
+
+}
