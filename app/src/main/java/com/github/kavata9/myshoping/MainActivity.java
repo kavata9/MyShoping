d1fc88826d15f74d9c58099101ed28bc61eada61
kavata9 <kavata24@gmail.com>
2018-07-02 18:56:43 +0300

    add butterknife dependency

--- app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
@@ -2,23 +2,30 @@ package com.github.kavata9.myshoping;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mLetsshopButton;
     private EditText mProductEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface PacificoFont = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
+        mAppNameTextView.setTypeface(PacificoFont);
         mProductEditText = (EditText) findViewById(R.id.ProductEditText);
         mLetsshopButton = (Button) findViewById(R.id.shoppingButton);
         mLetsshopButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/github/kavata9/myshoping/ShoppingActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/ShoppingActivity.java
@@ -3,17 +3,44 @@ package com.github.kavata9.myshoping;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ShoppingActivity extends Activity {
     private TextView mProductTextView;
+    private ListView mListView;
+    private String[] products = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mProductTextView = (TextView) findViewById(R.id.productTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, products);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String products = ((TextView)view).getText().toString();
+                Toast.makeText(ShoppingActivity.this, products, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String product = intent.getStringExtra("product");
     }
