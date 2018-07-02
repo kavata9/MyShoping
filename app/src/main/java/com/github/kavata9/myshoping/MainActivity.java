adbf9c271afd12d22584248fbde819c5b6811268
kavata9 <kavata24@gmail.com>
2018-07-02 11:26:16 +0300

    add main activity for our home page

--- app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/MainActivity.java
@@ -2,12 +2,28 @@ package com.github.kavata9.myshoping;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Button mLetsshopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLetsshopButton = (Button) findViewById(R.id.shoppingButton);
+        mLetsshopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Shopping Time!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
+
+
 }
