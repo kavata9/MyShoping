295864f78ea9a57ba89d22ce0f46820d61ce0a0a
kavata9 <kavata24@gmail.com>
2018-07-03 12:54:13 +0300

    add okhttp services

--- app/src/main/java/com/github/kavata9/myshoping/Constants.java
+++ app/src/main/java/com/github/kavata9/myshoping/Constants.java
@@ -1,8 +1,9 @@
 package com.github.kavata9.myshoping;
 
 public class Constants {
+
     public static final String WALMART_API_KEY = BuildConfig.WALMART_API_KEY;
     public static final String WALMART_BASE_URL = "http://api.walmartlabs.com/v1/search";
     public static final String PARAM_KEY = "apiKey";
-    public static final String MUSIC_QUERY_PARAMETER = "query";
+    public static final String WALMART_QUERY_PARAMETER = "query";
 }
