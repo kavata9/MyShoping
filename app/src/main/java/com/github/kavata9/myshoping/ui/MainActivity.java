c11fd0d1806147ec9009517b1416a9b8d8d22008
kavata9 <kavata24@gmail.com>
2018-07-06 13:09:37 +0300

    add firebase database

--- app/src/main/java/com/github/kavata9/myshoping/ui/MainActivity.java
+++ app/src/main/java/com/github/kavata9/myshoping/ui/MainActivity.java
@@ -14,14 +14,17 @@ import android.widget.Toast;
 
 import com.github.kavata9.myshoping.Constants;
 import com.github.kavata9.myshoping.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+//
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+private DatabaseReference mSearchedProductReference;
 
     @BindView(R.id.shoppingButton) Button mLetsshopButton;
     @BindView(R.id.ProductEditText) EditText mProductEditText;
@@ -29,37 +32,56 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        mSearchedProductReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_PRODUCT);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
 
         mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
         Typeface PacificoFont = Typeface.createFromAsset(getAssets(), "Pacifico.ttf");
 
         mAppNameTextView.setTypeface(PacificoFont);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
+
 
         mLetsshopButton.setOnClickListener(this);
 //        mLetsshopButton.setOnClickListener(new View.OnClickListener() {
     }
 
 
-            @Override
-            public void onClick(View v) {
-                String product = mProductEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, ShoppingListActivity.class);
-                addToSharedPreferences(product);
-                intent.putExtra("product", product);
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, "Shopping Time!", Toast.LENGTH_LONG).show();
-            }
+    @Override
+    public void onClick(View v) {
+        if(v == mLetsshopButton) {
+            String product= mProductEditText.getText().toString();
 
-    private void addToSharedPreferences(String product) {
-        mEditor.putString(Constants.PREFERENCES_PRODUCT_KEY, product).apply();
+            saveProductToFirebase(product);
+
+//            if(!(location).equals("")) {
+//                addToSharedPreferences(location);
+//            }
+
+            Intent intent = new Intent(MainActivity.this, ShoppingListActivity.class);
+            intent.putExtra("product", product);
+            startActivity(intent);
+        }
     }
+
+    public void saveProductToFirebase(String product) {
+        mSearchedProductReference.setValue(product);
+    }
+
+
+//    private void addToSharedPreferences(String product) {
+//        mEditor.putString(Constants.PREFERENCES_PRODUCT_KEY, product).apply();
+//    }
 }
 
 
