ea59dec52e1fa26397a6a0edb1a8ffe1b938f142
kavata9 <kavata24@gmail.com>
2018-07-06 11:53:58 +0300

    add changes

--- app/src/main/java/com/github/kavata9/myshoping/Constants.java
+++ app/src/main/java/com/github/kavata9/myshoping/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String PARAM_KEY = "apikey";
     public static final String WALMART_QUERY_PARAMETER = "query";
     public static final String PREFERENCES_PRODUCT_KEY = "product";
+    public static final String FIREBASE_CHILD_SEARCHED_PRODUCT = "searchedProduct";
 }
